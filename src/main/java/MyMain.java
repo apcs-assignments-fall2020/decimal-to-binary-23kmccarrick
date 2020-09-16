62d8c4f9c1937aeabab2bffb2a6150e93b0f394e
23kmccarrick <23kmccarrick@athenian.org>
2020-09-16 02:18:25 +0000

    finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,22 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
+        int result = 0;
+        int no = Integer.parseInt(binary);
+        for (int i = 0; i < binary.length(); i++){
+            if (no%10==1)
+                result += Math.pow(2,i);
+            no = no/10;
+        }
+        return result;
         // REPLACE WITH YOUR CODE HERE
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format:");
+        String input = scan.nextLine();
+        System.out.println("That is equal to the decimal value: " + binaryToDecimal(input));
     }
 }
